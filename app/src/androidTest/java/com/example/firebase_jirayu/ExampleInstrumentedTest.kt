007124bc212dab453466e8e95b98a8ce76e73c6d
kanyakornnn <153821839+kanyakornnn@users.noreply.github.com>
2024-02-02 23:17:19 +0700

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/firebase_jirayu/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/firebase_jirayu/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.firebase_jirayu", appContext.packageName)
+        assertEquals("com.example.firebase_kanyakorn", appContext.packageName)
     }
 }
