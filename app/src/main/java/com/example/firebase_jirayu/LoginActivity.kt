8899c0e80bd51681d676d7a1e386aa97df2399e6
kanyakornnn <153821839+kanyakornnn@users.noreply.github.com>
2024-02-02 23:18:15 +0700

    Update LoginActivity.kt

--- app/src/main/java/com/example/firebase_jirayu/LoginActivity.kt
+++ app/src/main/java/com/example/firebase_jirayu/LoginActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.firebase_jirayu.databinding.ActivityLoginBinding
+import com.example.firebase_kanyakorn.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
