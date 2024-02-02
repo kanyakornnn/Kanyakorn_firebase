2d67aeed56508f8bffac5690648c73ef0f48d2db
kanyakornnn <153821839+kanyakornnn@users.noreply.github.com>
2024-02-02 23:17:48 +0700

    Update SignupActivity.kt

--- app/src/main/java/com/example/firebase_jirayu/SignupActivity.kt
+++ app/src/main/java/com/example/firebase_jirayu/SignupActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.firebase_jirayu.databinding.ActivitySignupBinding
+import com.example.firebase_kanyakorn.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
